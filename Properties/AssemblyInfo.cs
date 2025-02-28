902ef1f749bc66267c28705fef2de8a011f4279e
! Sleepy <109904491+eepyfemboi@users.noreply.github.com>
2025-02-28 09:29:23 -0800

    e

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,1 +1,38 @@
-﻿
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using MelonLoader;
+using AmongUsHacks; // The namespace of your mod class
+// ...
+[assembly: MelonInfo(typeof(Class1), "AmongUsVRHacks", "0.1.1", "eepyfemboi")]
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AmongUsVRHacks")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AmongUsVRHacks")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bc0b30d4-82f9-43aa-b222-c5deba5d40ed")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
